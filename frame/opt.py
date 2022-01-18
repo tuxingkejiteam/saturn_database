c47cb4c9d16f6f2841544bfb594bdd9767d8f639
jokker <18761609908@163.com>
2022-01-18 10:19:45 +0800

    * 开始完善操作部分

---
+++ frame/opt.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8  -*-
+# -*- author: jokker -*-
+
+
+class Opt(object):
+
+
+    def add_json(self):
+        pass
+
+    def update_json(self):
+        pass
+
+    def del_json(self):
+        pass
+
+    def get_uc(self):
+        pass
+
+
+
+
+
+
+
