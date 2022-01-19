a8e6f57e345d39fb6df70df80c5470d3e783ccbc
steammeatball <zhangyi@tuxingkeji.com>
2022-01-19 09:06:05 +0800

    试着写mysql接口文档

--- db_tools/uc_md5.py
+++ db_tools/uc_md5.py
@@ -4,7 +4,7 @@ from db_tools.CRUD.MySQL_class import SaturnSQL
 # 数据库初始化
 mysql = SaturnSQL(host='192.168.3.101', user='root', password='root123', db_name='Saturn_Database')
 # 假装有一个list
-MD5_list = ['5a96a260a93dd004364be81589f1016f', 'f341a333a4bb011aa8bf0f2fce395048',
-            '0fbeb8aec7eb843811f9fce4462ce68d', 'wojiushiyishinengbunengyouxinduc']
-UC_list = mysql.get_uc_list([MD5_list])
+MD5_list = ['5a96a260a93dd004364be81589f10161', 'f341a333a4bb011aa8bf0f2fce395041',
+            '0fbeb8aec7eb843811f9fce4462ce681', 'wojiushiyishinengbunengyouxindu1']*100
+UC_list = mysql.get_uc_list(MD5_list)
 print(UC_list)
